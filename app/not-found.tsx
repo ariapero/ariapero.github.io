9362d4b7071717fafd514e94171f325043e32ecb
Ari <ariapero@mit.edu>
2024-11-15 01:44:01 -0500

    Custom 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link'
+import RadialBackground from '../components/RadialBackground'
  
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
-      <h2 className="text-2xl font-semibold text-gray-600 mb-4">Page Not Found</h2>
-      <p className="text-gray-500 mb-8">Oops! The page you're looking for doesn't exist.</p>
-      <Link href="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
-        Go back home
-      </Link>
-    </div>
+    <RadialBackground baseColor="#163734" highlightColor="#238177">
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="font-bold text-white -mb-8 font-reenie text-huge" style={{ textShadow: '2px 2px 4px rgba(0,0,0, 0.15)' }}>404</h1>
+            <h2 className="text-2xl font-semibold text-[#b4d0cb] mb-4" style={{ textShadow: '2px 2px 4px rgba(0,0,0, 0.5)' }}>Page Not Found</h2>
+            <p className="text-back-500 mb-8">Oops! The page you're looking for doesn't exist.</p>
+            <Link href="/" className="px-4 py-2 bg-white text-black rounded hover:bg-opacity-80 transition-colors">
+                Go back home
+            </Link>
+        </div>
+    </RadialBackground>
   )
 }
