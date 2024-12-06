c65b1007303ef9230306c755b2e25d0ac9a94039
Ari <ariapero@mit.edu>
2024-12-05 21:46:19 -0500

    Testing snap scroll mplementation on mobile works page

--- app/work/pageNoSnapScroll.tsx
+++ app/work/pageNoSnapScroll.tsx
@@ -19,7 +19,7 @@ const backgrounds = [
   '/rambax.png',
   '/civil.png',
   '/door.jpg',
-  '/textile.png',
+  '/textile.jpg',
 ]
 
 const projects = [
@@ -44,7 +44,7 @@ const content = [
     subtitle: "scroll down or use the links below to navigate"
   },
   {
-    title: "Web",
+    title: "Web",  // add iwrising, gis project
     subtitle: "web development and ui/ux design"
   },
   {
@@ -97,6 +97,7 @@ export default function WorksPage() {
   const [currentSlide, setCurrentSlide] = useState(0)
   const containerRef = useRef<HTMLDivElement>(null)
   const tickingRef = useRef(false)
+  const [isMobile, setIsMobile] = useState(false)
   const [showTopButton, setShowTopButton] = useState(false)
 
   const scrollSensitivitySetting = 30
@@ -129,17 +130,21 @@ export default function WorksPage() {
   }
 
   useEffect(() => {
-    const handleScroll = throttle((event: WheelEvent | TouchEvent) => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    const handleScroll = throttle((event: WheelEvent) => {
+      if (isMobile) return
       event.preventDefault()
       
       if (!tickingRef.current) {
-        let delta = 0;
-        if (event instanceof WheelEvent) {
-          delta = -event.deltaY;
-        } else if (event instanceof TouchEvent) {
-          const touch = event.touches[0];
-          delta = touch.clientY - (containerRef.current?.getBoundingClientRect().top || 0);
-        }
+        const delta: number = -event.deltaY
 
         if (delta <= -scrollSensitivitySetting) {
           tickingRef.current = true
@@ -155,9 +160,8 @@ export default function WorksPage() {
     }, 60)
 
     const container = containerRef.current
-    if (container) {
+    if (container && !isMobile) {
       container.addEventListener('wheel', handleScroll, { passive: false })
-      container.addEventListener('touchmove', handleScroll, { passive: false })
     }
 
     setShowTopButton(currentSlide > 0)
@@ -165,25 +169,33 @@ export default function WorksPage() {
     return () => {
       if (container) {
         container.removeEventListener('wheel', handleScroll)
-        container.removeEventListener('touchmove', handleScroll)
       }
     }
-  }, [currentSlide])
+  }, [currentSlide, isMobile])
 
   return (
-    <div ref={containerRef} className="h-screen overflow-y-auto overflow-x-hidden snap-y snap-mandatory">
+    // <div ref={containerRef} className="h-screen overflow-hidden">
+    <div ref={containerRef} className={`min-h-screen ${isMobile ? 'overflow-y-auto' : 'overflow-hidden'}`}>
       {backgrounds.map((bg, index) => (
         <section 
           key={index}
+          // className={`
+          //   fixed w-full bg-cover bg-no-repeat bg-center overflow-hidden will-change-transform backface-hidden
+          //   transition-transform duration-1200 ease-cubic-bezier
+          //   ${index === currentSlide ? 'z-10' : 'z-0'}
+          //   ${index < currentSlide ? 'down-scroll' : index > currentSlide ? 'up-scroll' : ''}
+          // `}
           className={`
-            fixed w-full h-screen bg-cover bg-no-repeat bg-center overflow-hidden will-change-transform backface-hidden
-            transition-transform duration-1200 ease-cubic-bezier snap-start
+            ${isMobile ? 'relative min-h-screen' : 'fixed w-full'}
+            bg-cover bg-no-repeat bg-center overflow-hidden will-change-transform backface-hidden
+            transition-transform duration-1200 ease-cubic-bezier
             ${index === currentSlide ? 'z-10' : 'z-0'}
-            ${index < currentSlide ? 'down-scroll' : index > currentSlide ? 'up-scroll' : ''}
+            ${!isMobile && (index < currentSlide ? 'down-scroll' : index > currentSlide ? 'up-scroll' : '')}
           `}
-          style={{
+          style={ !isMobile ? {
+            height: 'calc(100vh + 30vh)',
             transform: `translateY(${(index - currentSlide) * 100}vh)`,
-          }}
+          } : {}}
         >
           <Image
             src={bg}
@@ -195,28 +207,46 @@ export default function WorksPage() {
             loading={index === 0 ? "eager" : "lazy"}
           />
           <div className="absolute inset-0 bg-black bg-opacity-35"></div>
+          {/* <div className={`
+            h-screen flex justify-center items-center flex-col text-center text-white font-inter
+            transform transition-transform duration-[1700ms] ease-cubic-bezier
+            ${index < currentSlide ? 'translate-y-[40vh]' : index > currentSlide ? 'translate-y-[30vh]' : 'translate-y-[0vh]'}
+          `}> */}
           <div className={`
             h-screen flex justify-center items-center flex-col text-center text-white font-inter
+            ${!isMobile ? `
+              transform transition-transform duration-[1700ms] ease-cubic-bezier
+              ${index < currentSlide ? 'translate-y-[40vh]' : index > currentSlide ? 'translate-y-[30vh]' : 'translate-y-[0vh]'}
+            ` : ''}
           `}>
             {index === 0 ? (
               <h1
-                className="text-3xl md:text-[20vh] leading-tight font-sloop z-20"
+                className="z-20 text-[4vh] md:text-[20vh] leading-tight
+                  tracking-tighter md:tracking-normal
+                  uppercase md:capitalize
+                  font-medium md:font-normal
+                  font-zen md:font-sloop"
                 style={{ textShadow: '1px 1px 2px rgba(0,0,0, 0.15)' }}>
                   {content[index].title}
               </h1>
             ) : (
               <Link
                 href={projects[index]}
-                className="text-3xl md:text-[20vh] leading-tight font-sloop z-20 hover:opacity-75 transition-opacity"
+                className="z-20 text-[8vh] md:text-[20vh] leading-tight
+                  tracking-tighter md:tracking-normal
+                  uppercase md:capitalize
+                  font-medium md:font-normal
+                  font-zen md:font-sloop
+                  hover:opacity-75 transition-opacity"
                 style={{ textShadow: '1px 1px 2px rgba(0,0,0, 0.15)' }}>
                 {content[index].title}
               </Link>
             )}
-            <p className="text-xl md:text-2xl mt-4 font-zen font-medium">{content[index].subtitle}</p>
+            <p className="text-xl md:text-2xl mt-2 md:mt-4 font-zen font-medium z-10">{content[index].subtitle}</p>
             {index === 0 && (
-              <div className="mt-12 flex flex-col items-center">
+              <div className="mt-12 flex flex-col items-center z-10">
                 <p
-                  className="text-xl mb-0 font-bold uppercase font-zen"
+                  className="text-xl md:text-xl mb-0 font-bold uppercase font-zen"
                   style={{ textShadow: '1px 1px 1px rgba(0,0,0, 0.5)' }}>
                     Table of Contents
                 </p>
@@ -235,9 +265,9 @@ export default function WorksPage() {
           </div>
         </section>
       ))}
-      {showTopButton && (
+      {!isMobile && showTopButton && (
         <div 
-          className="fixed right-4 bottom-4 z-50 group"
+          className="fixed right-8 top-1/2 transform -translate-y-1/2 z-50 group"
           title="Back to Top"
         >
           <button
